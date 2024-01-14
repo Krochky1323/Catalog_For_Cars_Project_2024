a4fed9b67e4514fda844516d167abe8b0cec0d4c
raissa0122 <simeonovaraisa@gmail.com>
2024-01-15 00:16:12 +0200

    Add view model

---
+++ Catalog_For_Cars_Project_2024/ViewModels/CarFormViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Catalog_For_Cars_Project_2024.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog_For_Cars_Project_2024.ViewModels
+{
+    public class CarFormViewModel
+    {
+
+        //Това ще вижда клиента ни
+        public Car Car { get; set; }
+        public IEnumerable<Brand> Brands { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
