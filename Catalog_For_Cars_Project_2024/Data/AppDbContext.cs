0b9c222e6eee34b37b9dd52efb7678aae905345d
raissa0122 <simeonovaraisa@gmail.com>
2024-01-15 19:39:44 +0200

    Add AppDbContext

---
+++ Catalog_For_Cars_Project_2024/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿namespace Data
+{
+    using Catalog_For_Cars_Project_2024.Models;
+
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    //AppDbContext е предназначен да бъде използван като контекст на база данни в приложение, тъй като наследява IdentityDbContext
+    public class AppDbContext : IdentityDbContext
+    {
+
+        //Връзка на базата данни с моделите
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Car> Cars { get; set; }
+
+        public DbSet<Brand> Brands { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
