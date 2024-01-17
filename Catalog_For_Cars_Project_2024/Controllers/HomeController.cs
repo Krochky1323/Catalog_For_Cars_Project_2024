1255be64d03a5db8d44e8bff93dc6f10f3044358
raissa0122 <simeonovaraisa@gmail.com>
2024-01-17 16:55:24 +0200

    Update HomeController

--- Catalog_For_Cars_Project_2024/Controllers/HomeController.cs
+++ Catalog_For_Cars_Project_2024/Controllers/HomeController.cs
@@ -1,37 +1,17 @@
-﻿using Catalog_For_Cars_Project_2024.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Catalog_For_Cars_Project_2024.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
-            return View();
-        }
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Car");
+            }
 
-        public IActionResult Privacy()
-        {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
