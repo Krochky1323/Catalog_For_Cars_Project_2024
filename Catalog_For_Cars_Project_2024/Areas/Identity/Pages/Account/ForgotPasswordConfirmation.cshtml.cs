0bc0e7781c0289085282eac55ac3854e8e038052
raissa0122 <simeonovaraisa@gmail.com>
2024-01-17 18:23:39 +0200

    Add Login, Registration, Roles. Update Layout and litle fixes

---
+++ Catalog_For_Cars_Project_2024/Areas/Identity/Pages/Account/ForgotPasswordConfirmation.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Catalog_For_Cars_Project_2024.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class ForgotPasswordConfirmation : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
