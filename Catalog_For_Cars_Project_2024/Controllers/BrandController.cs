151cbfe7d6c972837715eddae6a73da40eed7e09
raissa0122 <simeonovaraisa@gmail.com>
2024-01-18 00:10:31 +0200

    Adding a 'stuffs' in Brand Controller and fix the layout

--- Catalog_For_Cars_Project_2024/Controllers/BrandController.cs
+++ Catalog_For_Cars_Project_2024/Controllers/BrandController.cs
@@ -1,6 +1,7 @@
 ﻿using Catalog_For_Cars_Project_2024.Data.Services.Brands;
 using Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,18 +11,17 @@ namespace Catalog_For_Cars_Project_2024.Controllers
 {
     public class BrandController : Controller
     {
-        private readonly IBrandService _service;
-
-        //Свързване на контролера вюто на бранда
-
-        public BrandController(IBrandService service)
+        private readonly AppDbContext _context;
+        public BrandController(AppDbContext context)
         {
-            _service = service;
+            _context = context;
         }
-        //public async Task<IActionResult> Index()
-        //{
-            //var data = await _service.GetAll();
-            //return View(data);
-        //}
+
+        //Свързване на контролера вюто на brands
+        public async Task<IActionResult> Index()
+        {
+            var allBrands = await _context.Brands.ToListAsync();
+            return View(allBrands);
+        }
     }
 }
